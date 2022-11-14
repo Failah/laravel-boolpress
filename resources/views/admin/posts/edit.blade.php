4bc420325002e4029caed124bc200ac5ab43f6d9
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-14 14:26:41 +0100

    added errors check in create and edit forms

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.dashboard')
 
 @section('content')
+    @if ($errors->any())
+        <h2>Form errors: please fill all spaces correctly!</h2>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="POST" action="{{ route('admin.posts.update', $post->id) }}">
         @csrf
         @method('PATCH')
         <div>
             <label for="title">Title:</label>
-            <input type="text" name="title" value="{{ old('title', $post->title) }}">
+            <input required maxlength="255" type="text" name="title" value="{{ old('title', $post->title) }}">
+            @error('title')
+                <div class="my-2 bg-danger text-white">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div>
             <label for="content">Content:</label>
-            <textarea name="content" cols="30" rows="10">{{ old('content', $post->content) }}</textarea>
+            <textarea required name="content" cols="30" rows="10">{{ old('content', $post->content) }}</textarea>
+            @error('content')
+                <div class="my-2 bg-danger text-white">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <input type="submit" value="Apply changes">
     </form>
