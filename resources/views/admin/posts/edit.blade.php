60b0ee331171d6cbc3890c5c2f0e916776c46523
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-15 13:57:14 +0100

    added category selection  in edit view

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -14,6 +14,8 @@
     <form method="POST" action="{{ route('admin.posts.update', $post->id) }}">
         @csrf
         @method('PATCH')
+
+        {{-- title selection --}}
         <div>
             <label for="title">Title:</label>
             <input required maxlength="255" type="text" name="title" value="{{ old('title', $post->title) }}">
@@ -23,6 +25,25 @@
                 </div>
             @enderror
         </div>
+
+        {{-- category selection --}}
+        <div>
+            <label for="category_id">Category:</label>
+            <select name="category_id">
+                @foreach ($categories as $category)
+                    <option value="{{ $category->id }}"
+                        {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>
+                        {{ $category->name }}</option>
+                @endforeach
+            </select>
+            @error('category_id')
+                <div class="my-2 bg-danger text-white">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
+        {{-- content definition --}}
         <div>
             <label for="content">Content:</label>
             <textarea required name="content" cols="30" rows="10">{{ old('content', $post->content) }}</textarea>
